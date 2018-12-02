e8acfb65afaf35fd30265cbe94cdf4a91cabdf84
chengchingwen <adgjl5645@hotmail.com>
2018-12-03 04:44:18 +0800

    the wrapper of statistic type as a BPE learner
    	save the config of learning a BPE map

---
+++ src/learn.jl
@@ -0,0 +1,58 @@
+using WordTokenizers
+using InternedStrings
+
+"simply the built-in split function for the origin tokenize method in subword-nmt"
+whitespace_tokenizer(str::AbstractString) = split(str)
+
+"regex escape from https://github.com/JuliaLang/julia/pull/29643"
+function _regex_escape(s::AbstractString)
+    res = replace(s, r"([()[\]{}?*+\-|^\$\\.&~#\s=!<>|:])" => s"\\\1")
+    replace(res, "\0" => "\\0")
+end
+
+function get_vocab(vfile::AbstractString)
+    vocab = Dict{String, Int}()
+    open(vfile) do io
+        for line ∈ eachline(io)
+            for word ∈ intern.(tokenize(line))
+                vocab[word] = get(vocab, word, 0) + 1
+            end
+        end
+    end
+    vocab
+end
+
+struct BPELearner
+    num_sym::Int
+    min_freq::Int
+    vfiles::Vector{String}
+
+    stats::Statistic
+    result::Vector{Pair{String, String}}
+
+    function BPELearner(vfiles::Vector{String}, num_sym::Int; min_freq::Int = 2)
+        vocab = mapreduce(get_vocab, merge!, vfiles)
+        stats = Statistic(vocab)
+        new(num_sym, min_freq, vfiles, stats, Vector{Pair{String, String}}(undef, num_sym))
+    end
+end
+
+function add!(bper::BPELearner, vfile::String)
+    push!(bper.vfiles, vfile)
+    nv = get_vocab(vfile)
+    update!(bper.stats, nv)
+end
+
+function learn!(bper::BPELearner)
+    if isassigned(bper.result)
+        bper.result = Vector(undef, bper.num_sym)
+    end
+
+    for i ∈ 1:bper.num_sym
+        mfp = most_freq(bper.stats)
+        get_freq(bper.stats, mfp) < bper.min_freq && (resize!(bper.result, i-1); break)
+        merge_pair!(bper.stats, mfp)
+        bper.result[i] = mfp
+    end
+end
+
