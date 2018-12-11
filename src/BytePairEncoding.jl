f62b4aecd9fe0397c5a5aa4c0db48f845723125e
chengchingwen <adgjl5645@hotmail.com>
2018-12-11 17:00:34 +0800

    add utf normalization to Module

--- src/BytePairEncoding.jl
+++ src/BytePairEncoding.jl
@@ -3,11 +3,13 @@ module BytePairEncoding
 using WordTokenizers
 using InternedStrings
 
+export normalize, UtfNomalizer
 export BPELearner, learn!, add!, emit
 export set_endsym, set_tokenizer, tokenize, whitespace_tokenize
 export isolate_gloss
 export Bpe, process_line, segment, segment_token
 
+include("./utfnorm.jl")
 include("./stats.jl")
 include("./learn.jl")
 include("./glossary.jl")
