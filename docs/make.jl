06e08ede24d31181735bef2c9595eeabef0fe6c0
chengchingwen <adgjl5645@hotmail.com>
2022-08-17 20:00:22 +0800

    update docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,23 @@
-using Documenter, BytePairEncoding
+using BytePairEncoding
+using Documenter
 
-makedocs(sitename="BytePairEncoding.jl",
-         pages = Any[
-           "Home"=>"index.md",
-         ],
-         )
-deploydocs(
-    repo = "github.com/chengchingwen/BytePairEncoding.jl.git",
+DocMeta.setdocmeta!(BytePairEncoding, :DocTestSetup, :(using BytePairEncoding); recursive=true)
+
+makedocs(;
+    modules=[BytePairEncoding],
+    authors="chengchingwen <adgjl5645@hotmail.com> and contributors",
+    repo="https://github.com/chengchingwen/BytePairEncoding.jl/blob/{commit}{path}#{line}",
+    sitename="BytePairEncoding.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://chengchingwen.github.io/BytePairEncoding.jl",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/chengchingwen/BytePairEncoding.jl",
 )
