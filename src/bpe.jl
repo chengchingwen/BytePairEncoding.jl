cb94d8a3f18b29ea9e3c23404d561c98134392fe
chengchingwen <adgjl5645@hotmail.com>
2022-08-17 17:10:36 +0800

    refine bpe code structure

--- src/bpe.jl
+++ src/bpe.jl
@@ -7,7 +7,42 @@ struct BPE <: AbstractBPE
     sepsym::Union{String, Nothing}
     endsym::Union{String, Nothing}
 end
-BPE(merging_rank; sepsym = nothing, endsym = nothing) = BPE(merging_rank, sepsym, endsym)
+BPE(merging_rank::Dict; sepsym = nothing, endsym = nothing) = BPE(merging_rank, sepsym, endsym)
+BPE(bpefile; sepsym = nothing, endsym = nothing, kws...) = BPE(read_merges(bpefile, endsym; kws...); sepsym, endsym)
+
+(bpe::BPE)(x) = bytepairencode(x, bpe.merging_rank, bpe.sepsym, bpe.endsym)
+
+function Base.show(io::IO, bpe::BPE)
+    print(io, "BPE(")
+    print(io, length(bpe.merging_rank))
+    print(io, " merges")
+    !isnothing(bpe.sepsym) && print(io, ", sepsym = ", bpe.sepsym)
+    !isnothing(bpe.endsym) && print(io, ", endsym = ", bpe.endsym)
+    print(io, ')')
+end
+
+struct NoBPE <: AbstractBPE end
+
+(bpe::NoBPE)(x) = [x]
+
+Base.show(io::IO, bpe::NoBPE) = print(io, "NoBPE()")
+
+struct CachedBPE{B <: AbstractBPE, D <: AbstractDict{String, Vector{String}}} <: AbstractBPE
+    bpe::B
+    cache::D
+end
+
+CachedBPE(bpe::AbstractBPE) = CachedBPE(bpe, Dict{String, Vector{String}}())
+
+function (bpe::CachedBPE)(x)
+    haskey(bpe.cache, x) && return bpe.cache[x]
+    y = bpe.bpe(x)
+    bpe.cache[x] = y
+    return y
+end
+
+Base.show(io::IO, bpe::CachedBPE) = (print(io, "CachedBPE("); show(io, bpe.bpe); print(io, ')'))
+
 
 _bigram(ms, i) where T = @inbounds ms[i], ms[i+1]
 
@@ -73,40 +108,12 @@ function merges(x::AbstractString, endsym = nothing)
   return buf
 end
 
-@inline bpe_postprocess(bpe::BPE, y::Merge) = as_string(y, bpe.sepsym, bpe.endsym)
-
-function (bpe::BPE)(x)
-  ms = merges(x, bpe.endsym)
-  length(ms) < 2 && return [bpe_postprocess(bpe, Merge(x, !isnothing(bpe.endsym)))]
-  y = merge_loop!(bpe.merging_rank, ms, x)
-  return map(Base.Fix1(bpe_postprocess, bpe), y)
+function bytepairencode(x, merging_rank, sepsym = nothing, endsym = nothing)
+    ms = merges(x, endsym)
+    if length(ms) < 2
+        y = [Merge(x, !isnothing(endsym))]
+    else
+        y = merge_loop!(merging_rank, ms, x)
+    end
+    return as_string.(y, sepsym, endsym)
 end
-
-struct CachedBPE{B <: AbstractBPE, D <: AbstractDict{String, Vector{String}}} <: AbstractBPE
-    bpe::B
-    cache::D
-end
-
-CacheBPE(bpe::AbstractBPE) = CacheBPE(bpe, Dict{String, Vector{String}}())
-
-function (bpe::CachedBPE)(x)
-    haskey(bpe.cache, x) && return bpe.cache[x]
-    y = bpe.bpe(x)
-    bpe.cache[x] = y
-    return y
-end
-
-function Base.show(io::IO, bpe::BPE)
-    print(io, "BPE(")
-    print(io, length(bpe.merging_rank))
-    print(io, " merges")
-    !isnothing(bpe.sepsym) && print(io, ", sepsym = ", bpe.sepsym)
-    !isnothing(bpe.endsym) && print(io, ", endsym = ", bpe.endsym)
-    print(io, ')')
-end
-
-struct NoBPE <: AbstractBPE end
-
-(bpe::NoBPE)(x) = [x]
-
-Base.show(io::IO, bpe::NoBPE) = print(io, "NoBPE()")
