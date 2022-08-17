a708f59a9a41037e6c945a2a8f64055791523dd1
chengchingwen <adgjl5645@hotmail.com>
2022-08-17 17:18:04 +0800

    update bpe test

--- test/test_bpe.jl
+++ test/test_bpe.jl
@@ -1,19 +1,32 @@
+using BytePairEncoding: read_merges
+using TextEncodeBase: FlatTokenizer, WordTokenization, Sentence, getvalue
+
+function process_line(tkr, line)
+    pat = r"\S.*\S"
+    m = match(pat, line)
+    isnothing(m) && return line
+    sentence = join(map(getvalue, tkr(Sentence(m.match))), ' ')
+    return replace(line, pat=>sentence)
+end
+
 @testset "bpe" begin
     infile = joinpath(dirname(@__FILE__), "data/corpus.en")
     bpefile = joinpath(dirname(@__FILE__), "data/bpe.out")
     refile = joinpath(dirname(@__FILE__), "data/corpus.bpe.ref.en")
 
-    bpe = Bpe(bpefile; sepsym="@@", endsym="")
+    merges = read_merges(bpefile, "</w>")
+    bpe = BPE(merges; sepsym="@@", endsym = "")
+    tkr = FlatTokenizer(BPETokenization(WordTokenization(tokenize = _python_whitespace_tokenizer), bpe))
 
     open(refile) do fr
         open(infile) do fi
             for (li, lr) ∈ zip(eachline(fi), eachline(fr))
-                @test process_line(bpe, li) == lr
+                @test process_line(tkr, li) == lr
             end
         end
     end
 
-    @test process_line(bpe, "  iron cement  \n") == "  ir@@ on c@@ ement  \n"
-    @test process_line(bpe, "iron" * "\ua0" * "cement\n") == "ir@@ on@@ "*"\ua0"*"@@ c@@ ement\n"
-    @test process_line(bpe, "\n") == "\n"
+    @test process_line(tkr, "  iron cement  \n") == "  ir@@ on c@@ ement  \n"
+    @test process_line(tkr, "iron" * "\ua0" * "cement\n") == "ir@@ on@@ "*"\ua0"*"@@ c@@ ement\n"
+    @test process_line(tkr, "\n") == "\n"
 end
