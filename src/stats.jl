2f9d6834bb008147bb2a2c1ade8b90442ce8bec8
chengchingwen <adgjl5645@hotmail.com>
2022-08-17 15:08:21 +0800

    refine bpe learning algo

--- src/stats.jl
+++ src/stats.jl
@@ -1,69 +1,75 @@
-toStrTuple(bpe::GenericBPE{T}, x::AbstractString) where T = Tuple(T.(merges(bpe, x)))
+#=
+  A minimum implementation of the byte pair encoding learning algorithm.
 
-struct Statistic{B<:GenericBPE}
-  bpe::B
-  vocab::Dict{Tuple, Int}
-  vkeys::Vector{Tuple}
-  pair_freq::Dict{Pair{String, String}, Int}
-  pair_index::Dict{Pair{String, String}, Vector{Int}}
+  Calculate the frequency of occurence of each sub-string pairs appears in vocabulary and
+    generate a lookup table of each pair where smaller number means higher merge priority.
+
+  Fields
+
+    pair_freq: a lookup table for getting the occurence frequency of a substring pair.
+
+    pair_index: a lookup table for getting a list of index of where the pair appears in.
+
+    vocab: a lookup table for getting the occurence frequency of a word (in the form of list of substrings)
+
+    vkeys: a list of words (in the form of list of substrings)
+=#
+
+struct Statistic
+    endsym::Union{String, Nothing}
+    vocab::Dict{Vector{String}, Int}
+    vkeys::Vector{Vector{String}}
+    pair_freq::Dict{Pair{String, String}, Int}
+    pair_index::Dict{Pair{String, String}, Vector{Int}}
 end
 
-Statistic(bper::BPELearner) = Statistic(bper.bpe, bper.vocabs)
-function Statistic(bpe::GenericBPE, v::Dict{String, Int})
-  vocab = Dict{Tuple, Int}((toStrTuple(bpe, k), v) for (k, v) ∈ pairs(v))
-  vkeys = collect(keys(vocab))
-  pair_freq = Dict{Pair{String, String}, Int}()
-  pair_index = Dict{Pair{String, String}, Vector{Int}}()
+function Statistic(word_counts::Dict{String, Int}, endsym)
+    vocab = Dict{Vector, Int}((bpe_units(word, endsym), freq) for (word, freq) ∈ pairs(word_counts))
+    vkeys = collect(keys(vocab))
+    pair_freq = Dict{Pair{String, String}, Int}()
+    pair_index = Dict{Pair{String, String}, Vector{Int}}()
 
-  for (i, tp) ∈ enumerate(vkeys)
-    for p ∈ bi_pairs(tp)
-      pair_freq[p] = get(pair_freq, p, 0) + vocab[tp]
-      indices = get!(pair_index, p, Int[])
-      push!(indices, i)
+    for (index, word_units) ∈ enumerate(vkeys)
+        for pair ∈ bi_pairs(word_units)
+            pair_freq[pair] = get(pair_freq, pair, 0) + vocab[word_units]
+            indices = get!(pair_index, pair, Int[])
+            push!(indices, index)
+        end
     end
-  end
 
-  Statistic(bpe, vocab, vkeys, pair_freq, pair_index)
+    Statistic(endsym, vocab, vkeys, pair_freq, pair_index)
 end
 
-"find adjacent element. return a list of Pair"
-function bi_pairs(stp)::Vector{Pair{String, String}}
-    init, rstp = Iterators.peel(stp)
-    map((x)->x[1]=>x[2], zip(stp, rstp))
-end
+# convert string into list of character units for later merging
+bpe_units(x::AbstractString, endsym) where T = as_string.(merges(x, endsym), nothing, endsym)
 
-"if only length one, return []"
-bi_pairs(stp::Tuple{String}) = Vector{Pair{String, String}}()
+# find adjacent element. return a list of Pair, if only length one, return []
+bi_pairs(t) = map(Base.splat(=>), zip(t, Iterators.drop(t, 1)))
 
+# most frequently pair
+Base.argmax(stats::Statistic) = argmax(stats.pair_freq)
 
-"most frequently pair"
-most_freq(stats::Statistic) = argmax(stats.pair_freq)
+# get frequency
+Base.getindex(stats::Statistic, p::Pair) = get(stats.pair_freq, p, 0)
 
-"get pair freq"
-get_freq(stats::Statistic, p::Pair) = get(stats.pair_freq, p, 0)
-
-"get word freq"
-get_freq(stats::Statistic, str::String) = get_freq(stats, toStrTuple(stats.bpe, str))
-get_freq(stats::Statistic, tp) = get(stats.vocab, tp, 0)
-
-"merge the gived pair and update Statistic"
+# merge the gived pair and update Statistic
 function merge_pair!(stats::Statistic, pair::Pair{String, String})
-    bpi = collect(stats.pair_index[pair])
-    sort!(bpi); unique!(bpi)
-    for index ∈ bpi
-        wtp = stats.vkeys[index]
-        freq = stats.vocab[wtp]
-        delete!(stats.vocab, wtp)
+    # migth have duplicate index becuase one word can have more than one occurence of a pair
+    word_w_pair = unique(stats.pair_index[pair])
+    for index ∈ word_w_pair
+        word_units = stats.vkeys[index]
+        freq = stats.vocab[word_units]
+        delete!(stats.vocab, word_units) # pair would be merged, this word units (w/o merged) doesn't exist anymore
 
-        bps = bi_pairs(wtp)
-        nwtp, indices = merged_pairs(bps, pair)
-        adjacents = adjacent_pairs(bps, indices)
-        mps = setdiff!(bi_pairs(nwtp), bps)
+        bipairs = bi_pairs(word_units)
+        new_word_units, merged_indices = merged_pairs(bipairs, pair)
+        adjacents = adjacent_pairs(bipairs, merged_indices)
+        merged_bipairs = setdiff!(bi_pairs(new_word_units), bipairs)
 
-        stats.vkeys[index] = nwtp
-        stats.vocab[nwtp] = freq
+        stats.vkeys[index] = new_word_units # update the word units to the new one (w/ merged)
+        stats.vocab[new_word_units] = freq  # set the frequency
 
-        update!(stats, index, nwtp, mps)
+        update!(stats, index, new_word_units, merged_bipairs)
         update!(stats, index, adjacents)
     end
 
@@ -72,105 +78,66 @@ function merge_pair!(stats::Statistic, pair::Pair{String, String})
     stats
 end
 
-"update Statistic of the new merged pair"
-function update!(stats::Statistic, index::Int, nwtp::Tuple, mps::Vector)
-    for mp ∈ mps
-        stats.pair_freq[mp] = get_freq(stats, mp) + stats.vocab[nwtp]
+# update Statistic of the new merged pair
+function update!(stats::Statistic, index::Int, new_word_units, merged_bipairs)
+    for mp ∈ merged_bipairs
+        stats.pair_freq[mp] = get(stats.pair_freq, mp, 0) + stats.vocab[new_word_units]
         indices = get!(stats.pair_index, mp, Int[])
         push!(indices, index)
     end
+    return
 end
 
-"update Statistic of the removed adjacent pairs"
-function update!(stats::Statistic, index::Int, adjacents::Vector)
+# update Statistic of the removed adjacent pairs
+function update!(stats::Statistic, index::Int, adjacents)
     for ap ∈ adjacents
         stats.pair_freq[ap] -= stats.vocab[stats.vkeys[index]]
         dindex = findfirst(isequal(index), stats.pair_index[ap])
         dindex !== nothing && deleteat!(stats.pair_index[ap], dindex)
     end
-    stats
+    return
 end
 
-"update a tuple"
-function update!(stats::Statistic, index::Int, nwtp::Tuple)
-    nbps = bi_pairs(nwtp)
-    for np ∈ nbps
-        stats.pair_freq[np] = get_freq(stats, np) + stats.vocab[nwtp]
-        indices = get!(stats.pair_index, np, Int[])
-        push!(indices, index)
-    end
-    stats
-end
+# get the adjacent pair of merged pair, they should be update to be adjacent to the new merged pair
+adjacent_pairs(bipairs::Array{Pair{String, String}}, indices::Vector{Int}) =
+    bipairs[filter!(∈(1:length(bipairs)), unique!(foldl((init,r)-> push!(init, r-1, r, r+1), indices; init = Int[])))]
 
+# merge the pair in the word tuple, return the new word units and indices where merge happened in the origin bipairs.
+function merged_pairs(bipairs::Array{Pair{String, String}}, pair::Pair{String, String})
+    bipairs = collect(bipairs)
+    new_pairs = String[]
+    indice = Int[]
 
-"update a word"
-function update!(stats::Statistic, str::String; freq::Int = 1)
-    stp = toStrTuple(stats.bpe, str)
-    if haskey(stats.vocab, stp)
-        stats.vocab[stp] += freq
-        for sbp ∈ bi_pairs(stp)
-            stats.pair_freq[sbp] += freq
-        end
-    else
-        stats.vocab[stp] = get_freq(stats.vocab, stp) + freq
-        push!(stats.vkeys, stp)
-        update!(stats, length(stats.vkeys), stp)
-    end
-    stats
-end
-
-"update a frequency vocab"
-function update!(stats::Statistic, vocab::Dict{String, Int})
-    for (word, freq) ∈ vocab
-        update!(stats, word; freq=freq)
-    end
-    stats
-end
-
-"get the adjacent pair of merged pair"
-adjacent_pairs(wtp::Tuple, pair::Pair{String, String}) = adjacent_pairs(bi_pairs(wtp), pair)
-function adjacent_pairs(_bps::Array{Pair{String, String}}, pair::Pair{String, String})
-    bps = collect(_bps)
-    indice = Vector{Int}()
-
-    merged = intern(join(pair))
-    for (i, x) ∈ enumerate(bps)
-        if 1<i && bps[i-1] == pair
-            bps[i] = merged=>x.second
+    merged = join(pair)
+    for (i, x) ∈ enumerate(bipairs)
+        if 1<i && bipairs[i-1] == pair
+            bipairs[i] = merged=>x.second
         elseif x == pair
-            push!(indice, i)
-        end
-    end
-    adjacent_pairs(_bps, indice)
-end
-
-function adjacent_pairs(bps::Array{Pair{String, String}}, indices::Vector{Int})
-    adjs = filter!((i)->1<=i<=length(bps), unique!(foldl((l,r)->(push!(l, r-1);push!(l, r);push!(l, r+1);l), indices, init=Int[])))
-    bps[adjs]
-end
-
-"merge the pair in the word tuple"
-merged_pairs(wtp::Tuple, pair::Pair{String, String}) = merged_pairs(bi_pairs(wtp), pair)
-function merged_pairs(bps::Array{Pair{String, String}}, pair::Pair{String, String})
-    bps = collect(bps)
-    nwss = Vector{String}()
-    indice = Vector{Int}()
-
-    merged = intern(join(pair))
-    for (i, x) ∈ enumerate(bps)
-        if 1<i && bps[i-1] == pair
-            bps[i] = merged=>x.second
-        elseif x == pair
-            push!(nwss, merged)
+            push!(new_pairs, merged)
             push!(indice, i)
             continue
         else
-            push!(nwss, x.first)
+            push!(new_pairs, x.first)
         end
 
-        if i == length(bps)
-            push!(nwss, x.second)
+        if i == length(bipairs)
+            push!(new_pairs, x.second)
         end
     end
-    Tuple(nwss), indice
+    new_pairs, indice
+end
+
+learn(word_counts, n_merge::Int, endsym::Union{Nothing, String} = nothing, min_freq::Int = 10) =
+    learn(argmax, word_counts, n_merge, endsym, min_freq)
+function learn(f, word_counts, n_merge::Int, endsym::Union{Nothing, String} = nothing, min_freq::Int = 10)
+    merging_rank = Dict{NTuple{2, Merge}, Int}()
+    pattern = isnothing(endsym) ? nothing : Regex("(.*)\\Q$endsym\\E\$")
+    stats = Statistic(word_counts, endsym)
+    for i = 1:n_merge
+        most_freq_pair = f(stats)
+        stats[most_freq_pair] < min_freq && break
+        merge_pair!(stats, most_freq_pair)
+        merging_rank[parse_merge(Tuple(most_freq_pair), pattern)] = i
+    end
+    return merging_rank
 end
