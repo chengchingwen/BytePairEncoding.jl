996870398c7643565ddccc6181acc0a68a9e18c4
chengchingwen <adgjl5645@hotmail.com>
2022-08-17 15:19:38 +0800

    update export

--- src/BytePairEncoding.jl
+++ src/BytePairEncoding.jl
@@ -3,19 +3,15 @@ module BytePairEncoding
 using TextEncodeBase
 using TextEncodeBase: CodeMap
 using WordTokenizers
-using InternedStrings
 
-export BPELearner, learn!, add!, emit
-export Bpe, process_line, segment, segment_token
+export BPELearner
+export BPE, BPETokenization
 
-export GenericBPE, ByteLevelBPE
-
-include("./mstring.jl")
-include("./bpe.jl")
+include("mstring.jl")
+include("bpe.jl")
 include("tokenization.jl")
-# include("./learn.jl")
-# include("./stats.jl")
-include("./defaults.jl")
+include("learn.jl")
+include("defaults.jl")
 # include("./api.jl")
 # include("./old_api.jl")
 
