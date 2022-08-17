508bdf516380cab61b92b91fd9318765c438aa33
chengchingwen <adgjl5645@hotmail.com>
2022-08-17 19:26:56 +0800

    remove old docs

--- docs/make.jl
+++ docs/make.jl
@@ -3,11 +3,6 @@ using Documenter, BytePairEncoding
 makedocs(sitename="BytePairEncoding.jl",
          pages = Any[
            "Home"=>"index.md",
-           "Guides" => Any[
-             "Encode" => "encode.md",
-             "Learning BPE" => "learn.md",
-             "Utilities" => "utils.md",
-           ]
          ],
          )
 deploydocs(
